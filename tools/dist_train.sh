b1ac19102fc8bd91e304b794987a89fabfaf140e
yera <yernar.zhetpissov@gmail.com>
2023-05-11 19:16:47 -0400

    updated dist_train.sh

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-CONFIG=${1:-local_configs/segformer/B1/segformer.b1.512x512.rarp50.160k.py}
+CONFIG=${1:-local_configs/segformer/B1/segformer.b1.512x512.rarp50.aug.160k.py}
 GPUS=${2:-1}
 PORT=${PORT:-29500}
+CHECKPOINT=${3:-work_dirs/segformer.b1.512x512.endvis2018.80k/iter_44000.pth}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 python3 -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+    $(dirname "$0")/train.py $CONFIG --load-from $CHECKPOINT --launcher pytorch ${@:3}
